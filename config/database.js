d60a3f6d1ae87e95e0dd9e49b585f6e175dbf439
Mikey Neely <mike.neely90@gmail.com>
2019-08-19 12:14:03 -0400

    second commit

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : `mongodb+srv://${process.env.userName}:${process.env.password}@crud1-68o08.mongodb.net/${process.env.database}?retryWrites=true&w=majority',`
+    'url' : `mongodb+srv://${process.env.userName}:${process.env.password}@crud1-68o08.mongodb.net/${process.env.database}?retryWrites=true&w=majority`,
     'dbName': 'userInfo'
 };
